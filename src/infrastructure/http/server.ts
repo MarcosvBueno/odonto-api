11183c4d82724d9112dcd0d7b0d0fe12f2a98065
MarcosvBueno <mvbueno07@gmail.com>
2025-04-14 09:56:12 -0300

    feat: add zod validations for envs info

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import 'express-async-errors'
-
+import { env } from '../env'
+import { ErrorHandler } from '../middleware/error-handler'
 const app = express()
 
 app.use(express.json())
@@ -11,6 +12,8 @@ app.use('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.use(ErrorHandler)
 app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+  console.log(`Server is running on port ${env.PORT} 🚀`)
+  console.log(`${env.API_URL}`)
 })
